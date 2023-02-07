c3d2a087362af30bd02156988ca3bc33946de135
Sergei Shlegel <serg-69@mail.ru>
2023-02-07 23:35:23 +0300

    Added function

--- app/src/main/java/com/example/diceplay/MainActivity.kt
+++ app/src/main/java/com/example/diceplay/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.example.diceplay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.diceplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage : androidx.appcompat.widget.AppCompatImageView
-    lateinit var diceImage2: androidx.appcompat.widget.AppCompatImageView
     lateinit var bindigClass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindigClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindigClass.root)
-        bindigClass.buttonClick.setOnClickListener { rollDice() }
-
-        diceImage = bindigClass.dice
-        diceImage2 = bindigClass.dice2
+        bindigClass.buttonClick.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
     }
-    fun rollDice(){
 
+    private fun rollDice(){
+        bindigClass.dice.setImageResource(getRandomImage())
+        bindigClass.dice2.setImageResource(getRandomImage())
     }
-    fun getRandomDiceImage(){
+    private fun getRandomImage(): Int {
         val randomInt = (1..6).random()
-        when(randomInt){
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
